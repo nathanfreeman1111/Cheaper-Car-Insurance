370ea98726e3d04ee5857dfd298737a7b9033321
nathanfreeman1111 <65598543+nathanfreeman1111@users.noreply.github.com>
2022-10-18 18:50:19 +0530

    Initial commit

---
+++ spec/cheaper-car-insurance-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CheaperCarInsuranceView from '../lib/cheaper-car-insurance-view';
+
+describe('CheaperCarInsuranceView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
